7d93ae0dde7cce20aa42c78c44452509233bc56a
apblauzon <apblauzon@gmail.com>
2024-10-21 20:46:00 +0800

    Update mobile.py

--- mobile.py
+++ mobile.py
@@ -16,7 +16,12 @@ try:
 
         st.info(f"Is session on a PC? {st.session_state.is_session_pc}")
         st.info(f"Is the browser embedded in Facebook? {is_facebook_embedded}")
+
+        # Check conditions and create a link for Google Chrome download
+        if not st.session_state.is_session_pc and is_facebook_embedded:
+            st.warning("You are using a Facebook embedded browser on a mobile device. For a better experience, consider using Google Chrome.")
+            st.markdown("[Download Google Chrome](https://test-mobile.streamlit.app/)", unsafe_allow_html=True)
     else:
-        pass
+       pass
 except Exception as e:
     pass
