f19b14a6863ade930c39f5d4ba42d6aef36090f2
apblauzon <apblauzon@gmail.com>
2024-10-21 21:10:34 +0800

    Update mobile.py

--- mobile.py
+++ mobile.py
@@ -16,7 +16,7 @@ try:
         if not st.session_state.is_session_pc and is_facebook_embedded:
             st.markdown(
                 """
-                <div style='text-align: center; background-color: #ffe5b4; border: 2px solid #ffcc80; padding: 10px; border-radius: 5px;'>
+                <div style='text-align: center; background-color: #fff; border: 2px solid #ffcc80; padding: 10px; border-radius: 5px;'>
                     <h5 style='margin: 0; color: orange;'>You are using a Facebook embedded browser on a mobile device. For a better experience, consider using Google Chrome browser.</h5>
                 </div>
                 """,
